00b1879055f719a1925054c94c882286fff69b40
Rafael540 <alves540@hotmail.com>
2025-07-16 13:17:16 -0300

    Greeting

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,13 @@ import Invoices from './routes/Invoices/index.tsx'
 import NotFunding from './routes/NotFounding/index.tsx'
 import Invoice from './routes/Invoices/Invoice/index.tsx'
 import InvoicesIndex from './routes/Invoices/Invoicesindex/index.tsx'
+import Greeting from './routes/Greeting/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
+        <Route index element={<Greeting/>}/>
         <Route path="expenses" element={<Expenses />} />
         <Route path="invoices" element={<Invoices />} >
           <Route index element={<InvoicesIndex/>} />
---
+++ src/routes/Greeting/index.tsx
@@ -0,0 +1,7 @@
+export default function Greeting(){
+    return(
+        <main style={{padding: "1rem"}}>
+            <p>Bem-vindo</p>
+        </main>
+    );
+}
